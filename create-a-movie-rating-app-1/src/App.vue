e8c3f41937cbfe20032993a59a9b468f1938629f
Olga Golubev <olgagolubev.pro@gmail.com>
2025-01-09 17:58:20 +0200

    init

---
+++ create-a-movie-rating-app-1/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import { items } from "./movies.json";
+import MovieCard from "@/components/MovieCard.vue"
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+// import { StarIcon } from "@heroicons/vue/24/solid";
+
+const movies = ref(items)
+</script>
+
+<template>
+  <!-- This is where your template goes	-->
+  <div class="bg-gray-950 w-full flex flex-col gap-5 justify-center p-4 overflow-y-scroll overflow-x-hidden min-h-screen">
+    <h1 class="text-white text-center text-5xl py-4">Movies</h1>
+    <ul class="flex flex-wrap gap-4 content-start justify-center">
+      <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie"/>
+    </ul>
+  </div>
+</template>
---
+++ create-a-movie-rating-app-1/src/components/MovieCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  movie: Object,
+})
+
+const {name, description, image, rating, genres, inTheaters} = props.movie;
+</script>
+
+<template>
+    <div class="bg-white flex flex-col gap-2 max-w-80 rounded-2xl overflow-hidden border ">
+        <div class="h-96 relative">
+            <img :src="image" :alt="name" class="h-full w-full object-cover">
+        </div>
+        <div class="p-4 flex flex-col justify-between h-full gap-2">
+            <div>
+                <h2 class="font-bold mb-2">{{ name }}</h2>
+                <p class="mb-4">{{ description }}</p>
+            </div>
+            <div>
+                <span class="inline mr-2 bg-indigo-500 py-1 px-2 rounded-2xl text-white" v-for="g in genres" :key="g">{{ g }}</span>
+                <div class="mt-2">
+                    Raiting: ({{ rating }}/5) <span v-for="n in rating">⭐️</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
