e652793a9ee7162491da97b48b1d5d8a278b41cb
Olga Golubev <olgagolubev.pro@gmail.com>
2025-01-23 16:27:15 +0200

    tic-tac init

---
+++ tic-tac-toe-1/tests/app.test.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import App from "../src/App.vue";
+import { mount } from "@vue/test-utils";
+
+let wrapper = null;
