b34f91c16b6cb0ed63dd34c063bcb66842ff31ad
Olga Golubev <olgagolubev.pro@gmail.com>
2025-02-07 18:06:04 +0200

    stupid sweam

--- virtual-aquarium-1/src/Fish.vue
+++ virtual-aquarium-1/src/Fish.vue
@@ -1,13 +1,45 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import { fishMenu } from "./fishMenu";
 const props = defineProps(['fish'])
 
 const imageSrc = fishMenu.find(item => item.type === props.fish.fishType)?.src;
 
-const startCoordinates = { 
-    x: Math.floor(Math.random()*100),
-    y: Math.floor(Math.random()*100),
-}
+const startCoordinates = ref({ 
+    x: Math.floor(Math.random()*80) + 10,
+    y: Math.floor(Math.random()*80) + 10,
+})
+
+let verticalDirection = "up";
+let horizontalDirection = ref("left");
+let intervalId = null;
+
+const moveFish = () => {
+  if (intervalId) clearInterval(intervalId);
+
+  intervalId = setInterval(() => {
+    if (verticalDirection === "up") {
+      startCoordinates.value.y--;
+      if (startCoordinates.value.y <= 10) verticalDirection = "down";
+    } else {
+      startCoordinates.value.y++;
+      if (startCoordinates.value.y >= 90) verticalDirection = "up";
+    }
+
+    if (horizontalDirection.value === "left") {
+      startCoordinates.value.x--;
+      if (startCoordinates.value.x <= 10) horizontalDirection.value = "right";
+    } else {
+      startCoordinates.value.x++;
+      if (startCoordinates.value.x >= 90) horizontalDirection.value = "left";
+    }
+  }, 100);
+};
+
+onMounted(() => {
+        console.log(startCoordinates.value)
+        moveFish()
+})
 
 </script>
 
@@ -15,6 +47,7 @@ const startCoordinates = {
     <div>
         <button 
             class="fish"
+            :class="{'left': horizontalDirection === 'left' }"
             :style="{'top': startCoordinates.y + '%', 'left': startCoordinates.x + '%'}"
             @click.prevent="handleFishClick(fish)">
             <img :src="imageSrc"/>
@@ -28,6 +61,9 @@ const startCoordinates = {
     .fish {
         max-width: 100px;
         position: absolute;
+    }
+    .left {
+        transform: scaleX(-1);
     }
     .fish:hover {
         border: 2px, solid, yellowgreen;
